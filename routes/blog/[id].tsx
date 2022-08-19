3fda61c755eac8b4abe41da9b2c5c23febda0ffb
sairaj <sairaj@findcoder.io>
2022-08-19 18:31:34 +0530

    parse user preferred lang for date format

--- routes/blog/[id].tsx
+++ routes/blog/[id].tsx
@@ -4,8 +4,13 @@ import { Handlers, PageProps } from "$fresh/server.ts";
 import { tw } from "@twind";
 import { loadPost, Post } from "../../utils/posts.ts";
 import { render, CSS } from "https://deno.land/x/gfm@0.1.22/mod.ts";
+import { State } from "../../utils/state.ts";
 
-export const handler: Handlers<Post> = {
+interface Data extends State {
+  post: Post;
+}
+
+export const handler: Handlers<Data, State> = {
   GET: async (_req, ctx) => {
     const id = ctx.params.id;
     const post = await loadPost(id);
@@ -13,14 +18,17 @@ export const handler: Handlers<Post> = {
       return new Response("Post not found", { status: 404 });
     }
 
-    return ctx.render(post);
+    return ctx.render({
+      ...ctx.state,
+      post,
+    });
   },
 };
 
-export default function BlogPostPage(props: PageProps<Post>) {
-  const post = props.data;
+export default function BlogPostPage(props: PageProps<Data>) {
+  const { post, locales } = props.data;
   const html = render(post.content);
-  const dateFmt = new Intl.DateTimeFormat("en-UK", {
+  const dateFmt = new Intl.DateTimeFormat(locales, {
     dateStyle: "full",
   });
 
