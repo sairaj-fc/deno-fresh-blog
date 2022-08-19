b763d8e27579ebadafb4fabf559e1a2636458397
sairaj <sairaj@findcoder.io>
2022-08-19 16:11:31 +0530

    add dynamic post handler

--- routes/blog/[id].tsx
+++ routes/blog/[id].tsx
@@ -1,24 +1,29 @@
 /** @jsx h */
 import { h } from "preact";
+import { Handlers, PageProps } from "$fresh/server.ts";
 import { tw } from "@twind";
-import type { Post } from "../../utils/posts.ts";
+import { loadPost, Post } from "../../utils/posts.ts";
 
-const post: Post = {
-  id: "hello",
-  title: "Hello World",
-  publishAt: new Date(),
-  snippet: "This is the snippet",
-  content: "This is my first post.",
+export const handler: Handlers<Post> = {
+  GET: async (_req, ctx) => {
+    const id = ctx.params.id;
+    const post = await loadPost(id);
+    if (!post) {
+      return new Response("Post not found", { status: 404 });
+    }
+
+    return ctx.render(post);
+  },
 };
 
-export default function BlogPostPage() {
+export default function BlogPostPage(props: PageProps) {
+  const post = props.data;
+
   return (
     <div class={tw`px-4 mx-auto max-w-screen-md mt-6`}>
       <p class={tw`text-gray-600`}>{post.publishAt.toLocaleDateString()}</p>
       <h1 class={tw`text-5xl mt-2 font-bold`}>{post.title}</h1>
-      <div class={tw`mt-12`}>
-        {post.content}
-      </div>
+      <div class={tw`mt-12`}>{post.content}</div>
     </div>
   );
 }
