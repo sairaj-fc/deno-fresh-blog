b6d93c0f4870047f4d4c1bee9b82ee472fe3e7bd
sairaj <sairaj@findcoder.io>
2022-08-19 16:50:00 +0530

    index page

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,11 +1,43 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
+import { listPosts, Post } from "../utils/posts.ts";
+import { Handlers, PageProps } from "$fresh/server.ts";
 
-export default function Home() {
+export const handler: Handlers<Post[]> = {
+  async GET(req, ctx) {
+    const posts = await listPosts();
+    return ctx.render(posts);
+  },
+};
+
+export default function Home(props: PageProps<Post[]>) {
+  const posts = props.data;
+
   return (
-    <div class={tw`text-5xl`}>
-      Hello World
+    <div class={tw`px-4 mx-auto max-w-screen-md mt-6`}>
+      <h1 class={tw`text-5xl mt-12 font-bold`}>Sairaj's Blog</h1>
+      <ul class={tw`mt-8`}>
+        {posts.map((post) => (
+          <PostEntry key={post.id} post={post} />
+        ))}
+      </ul>
     </div>
+  );
+}
+
+function PostEntry({ post }: { post: Post }) {
+  return (
+    <li class={tw`border-t`}>
+      <a href={`/blog/${post.id}`} class={tw`py-4 flex gap-4 group`}>
+        <div>{post.publishAt.toLocaleDateString()}</div>
+        <div>
+          <h2 class={tw`font-bold text-xl group-hover:underline`}>
+            {post.title}
+          </h2>
+          <p class={tw`text-gray-600`}>{post.snippet}</p>
+        </div>
+      </a>
+    </li>
   );
 }
