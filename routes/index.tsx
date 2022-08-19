4d14bd784f2d29513a4ad992b75661e0375ef1ca
sairaj <sairaj@findcoder.io>
2022-08-19 14:29:15 +0530

    basic scafolding

---
+++ routes/index.tsx
@@ -0,0 +1,11 @@
+/** @jsx h */
+import { h } from "preact";
+import { tw } from "@twind";
+
+export default function Home() {
+  return (
+    <div class={tw`text-5xl`}>
+      Hello World
+    </div>
+  );
+}
